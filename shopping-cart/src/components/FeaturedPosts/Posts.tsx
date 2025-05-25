7c727fc37891b6405db475d78018b20d8cb2ec35
ezzzinne <cynthia.nwani017@gmail.com>
2025-05-26 00:36:57 +0100

    changed format of pictures from png to jpg for compatibility

--- shopping-cart/src/components/FeaturedPosts/Posts.tsx
+++ shopping-cart/src/components/FeaturedPosts/Posts.tsx
@@ -1,7 +1,7 @@
 import styles from './Posts.module.css';
-import Post1 from '../../assets/post 1.png';
-import Post2 from '../../assets/post 2.png';
-import Post3 from '../../assets/post 3.png';
+import Post1 from '../../assets/post 1.jpg';
+import Post2 from '../../assets/post 2.jpg';
+import Post3 from '../../assets/post 3.jpg';
 import Clock from '../../assets/clock.svg';
 import Comments from '../../assets/comments.svg';
 import Arrow from '../../assets/arrow.svg';
